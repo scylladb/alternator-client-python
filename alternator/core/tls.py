b66f06e241df48ad5724cede486c86f47d791136
Dmitry Kropachev <dmitry.kropachev@gmail.com>
2026-06-26 18:12:42 -0400

    Honor custom-only TLS trust roots (#80)

--- alternator/core/tls.py
+++ alternator/core/tls.py
@@ -19,26 +19,24 @@ def create_ssl_context(tls_config: TLS) -> ssl.SSLContext:
     Returns:
         Configured SSL context
     """
-    context = ssl.create_default_context()
     if tls_config.trust_all_certificates:
+        context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
         # INSECURE - for development only
         context.check_hostname = False
         context.verify_mode = ssl.CERT_NONE
     else:
+        if tls_config.trust_system_ca_certs:
+            context = ssl.create_default_context()
+        else:
+            context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+            context.verify_mode = ssl.CERT_REQUIRED
+
         # Configure hostname verification
         context.check_hostname = tls_config.verify_hostname
 
     if not tls_config.trust_all_certificates and tls_config.custom_ca_cert_paths:
         for cert_path in tls_config.custom_ca_cert_paths:
             context.load_verify_locations(str(cert_path))
-
-    # If not using system CA and custom certs provided, don't load system certs
-    if (
-        not tls_config.trust_all_certificates
-        and not tls_config.trust_system_ca_certs
-        and tls_config.custom_ca_cert_paths
-    ):
-        context.verify_mode = ssl.CERT_REQUIRED
 
     if tls_config.client_cert_path is not None:
         context.load_cert_chain(
